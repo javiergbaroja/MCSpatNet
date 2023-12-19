0de128ef74fa9b7bb9bceab97e2e681f803f515b
Garcia Baroja Javier (jg23p152) <jg23p152@submit01.ubelix.unibe.ch>
2023-12-19 10:51:54 +0100

    last minor changes

--- 01_train_mcspat_custom.py
+++ 01_train_mcspat_custom.py
@@ -7,6 +7,9 @@ import glob
 import math
 from tqdm import tqdm
 from natsort import natsorted
+from termcolor import colored
+from collections import Counter
+import time
 
 import numpy as np
 import torch
@@ -15,15 +18,18 @@ from torch.utils.data import DataLoader
 
 import cv2
 from skimage import filters
-from skimage.measure import label, moments
+from skimage.measure import label, moments, regionprops_table
 
 from cluster_helper import *
-from utils import get_train_args, create_logger, empty_trash, seed_everything, get_npy_files
+from utils import get_train_args, create_logger, empty_trash, seed_everything
 from model_arch_custom import UnetVggMultihead_custom as UnetVggMultihead
 
-from my_dataloader_w_kfunc_custom import CellsDataset
+from my_dataloader_w_kfunc_custom import CellsDataset_train, CellsDataset_test
 from my_dataloader_custom import CellsDataset as CellsDataset_simple
 from cluster_helper_eff import *
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from hover_net.metrics.stats_utils import pair_coordinates
 
 feature_code = {'decoder':0, 'cell-detect':1, 'class':2, 'subclass':3, 'k-cell':4}
 
@@ -57,14 +63,13 @@ if __name__=="__main__":
         if os.path.isdir(cluster_tmp_out):
             shutil.rmtree(cluster_tmp_out)
     else:
-        chekpoints = natsorted(get_npy_files(checkpoints_save_path, 'pth'))
-        args.model_param_path = chekpoints[-1] if len(chekpoints) > 0 else None
+        chekpoint = os.path.join(checkpoints_save_path, 'mcspat_last.pth')
+        args.model_param_path = chekpoint if os.path.isfile(chekpoint) else None
 
     os.makedirs(args.checkpoints_root_dir, exist_ok=True)
     os.makedirs(checkpoints_save_path, exist_ok=True)
     os.makedirs(args.clustering_pseudo_gt_root, exist_ok=True)
     os.makedirs(cluster_tmp_out, exist_ok=True)
-
     
     start_epoch             = 0  # To use if continuing training from a previous epoch loaded from model_param_path
     epoch_start_eval_prec   = 0 # After epoch_start_eval_prec epochs start to evaluate F-score of predictions on the validation set.
@@ -76,20 +81,10 @@ if __name__=="__main__":
     seed_everything(args.seed) # set seed for reproducibility
     
     # Configure training dataset
-    train_image_root = os.path.join(args.train_data_root, 'images')
-    train_dmap_root = os.path.join(args.train_data_root, 'gt_custom') 
-    train_dots_root = os.path.join(args.train_data_root, 'gt_custom') 
     train_dmap_subclasses_root = cluster_tmp_out
-    train_dots_subclasses_root = train_dmap_subclasses_root
-    train_kmap_root = os.path.join(args.train_data_root, 'k_func_maps') 
 
     # Configure validation dataset
-    test_image_root = os.path.join(args.test_data_root, 'images')
-    test_dmap_root = os.path.join(args.test_data_root, 'gt_custom')
-    test_dots_root = os.path.join(args.test_data_root, 'gt_custom')
     test_dmap_subclasses_root = cluster_tmp_out
-    test_dots_subclasses_root = test_dmap_subclasses_root
-    test_kmap_root = os.path.join(args.test_data_root, 'k_func_maps') 
 
     dropout_prob = 0.2
     initial_pad = 126 # We add padding so that final output has same size as input since we do not use same padding conv.
@@ -101,7 +96,7 @@ if __name__=="__main__":
     n_classes_out = n_classes + 1 # number of output classes = number of cell classes (lymphocytes, tumor, stromal) + 1 (for cell detection channel)
     class_indx = '1,2,3' # the index of the classes channels in the ground truth
     n_clusters = 5 # number of clusters per class
-    n_classes2 = n_clusters * (n_classes) # number of output classes for the cell cluster classification
+    n_classes_deep_clustering = n_clusters * n_classes # number of output classes for the cell cluster classification
 
     prints_per_epoch=1 # print frequency per epoch
 
@@ -110,7 +105,7 @@ if __name__=="__main__":
     r_range = range(0, 100, r_step)
     r_arr = np.array([*r_range])
     r_classes = len(r_range) # number of output channels for the K function for a single class
-    r_classes_all = r_classes * (n_classes ) # number of output channels for the K function over all classes
+    k_func_dim = r_classes * n_classes # number of output channels for the K function over all classes
 
     k_norm_factor = 100 # the maximum K-value (i.e. number of nearby cells at radius r) to normalize the K-func to [0,1]
     lamda_class = args.lamda_class;  # weight for dice loss for cell classification
@@ -125,7 +120,9 @@ if __name__=="__main__":
                                                    'n_classes':n_classes, 
                                                    'n_channels':n_channels, 
                                                    'n_heads':4, 
-                                                   'head_classes':[1, n_classes, n_classes2, r_classes_all]}))
+                                                   'head_classes':[1, n_classes, n_classes_deep_clustering, k_func_dim],
+                                                   'use_dice_loss':args.use_dice_loss,
+                                                   'use_ce_loss':args.use_ce_loss}))
     if args.model_param_path is not None:
         if args.model_param_path.split('_')[-2].isnumeric():
             start_epoch = int(args.model_param_path.split('_')[-2])
@@ -141,7 +138,7 @@ if __name__=="__main__":
     
     model.to(device)
     model.module.control_encoder_and_bottleneck(order='freeze', print_info=True)
-    logger.info(f'------Loaded model from {args.model_param_path}. Encoder and bottleneck layers are frozen.')
+    logger.info(f'------Loaded model from {args.model_param_path} [epoch {start_epoch}]. Encoder and bottleneck layers are frozen.')
 
     # Initialize sigmoid layer for cell detection
     criterion_sig = nn.Sigmoid()
@@ -151,39 +148,34 @@ if __name__=="__main__":
     criterion_l1_sum = nn.L1Loss(reduction='sum')
 
     # Initialize Optimizer
-    # optimizer=torch.optim.Adam(list(model.final_layers_lst.parameters())+list(model.decoder.parameters())+list(model.bottleneck.parameters())+list(model.encoder.parameters()),lr)
-    # optimizer=torch.optim.Adam(list(model.module.final_layers_lst.parameters())+list(model.module.decoder.parameters())+list(model.module.bottleneck.parameters())+list(model.module.encoder.parameters()),args.lr)
-    optimizer=torch.optim.Adam(model.parameters(),args.lr)
+    optimizer=torch.optim.Adam(model.parameters(), args.lr[0])
 
     # Initialize training dataset loader
-    train_dataset=CellsDataset(args.root_dir,class_indx, train_dmap_subclasses_root, split_filepath=args.train_split_filepath, phase='train', fixed_size=args.crop_size, max_scale=16)
+    train_dataset=CellsDataset_train(args.root_dir,class_indx, train_dmap_subclasses_root, split_filepath=args.train_split_filepath, phase='train', fixed_size=args.crop_size, max_scale=16)
     train_loader=DataLoader(train_dataset, batch_size=args.batch_size[0], shuffle=True, num_workers=14, worker_init_fn=worker_init_fn)
 
     # Validation set does not crop images (fixed_size=-1) --> they are of different sizes --> batch size must be 1
-    test_dataset=CellsDataset(args.root_dir, class_indx, test_dmap_subclasses_root, split_filepath=args.test_split_filepath,phase='test', fixed_size=-1, max_scale=16)
-    test_loader=DataLoader(test_dataset, batch_size=int(args.batch_size[0]*1.5), shuffle=False, num_workers=14, worker_init_fn=worker_init_fn)
+    test_dataset=CellsDataset_test(args.root_dir, class_indx, split_filepath=args.test_split_filepath, phase='test', fixed_size=-1, max_scale=16)
+    test_loader=DataLoader(test_dataset, batch_size=int(args.batch_size[0]*4/3), shuffle=False, num_workers=14, worker_init_fn=worker_init_fn)
 
     # Initialize training dataset loader for clustering phase
     simple_train_dataset=CellsDataset_simple(args.root_dir, class_indx, split_filepath=args.cluster_data_filepath, phase='test', fixed_size=-1, max_scale=16, return_padding=True)
-    simple_train_loader=DataLoader(simple_train_dataset, batch_size=int(args.batch_size[0]*1.5), shuffle=False, num_workers=14, worker_init_fn=worker_init_fn)
+    simple_train_loader=DataLoader(simple_train_dataset, batch_size=int(args.batch_size[0]*4/3), shuffle=False, num_workers=14, worker_init_fn=worker_init_fn)
 
-    logger.info(f'------Training set size: {len(train_dataset)} image crops. Dataloader will have {len(train_loader)} batches of size {train_loader.batch_size}.')
-    logger.info(f'------Clustering set size: {len(simple_train_dataset)} image crops. Dataloader will have {len(simple_train_loader)} batches of size {simple_train_loader.batch_size}.')
-    logger.info(f'------Validataion set size: {len(test_dataset)} image crops. Dataloader will have {len(test_loader)} batches of size {test_loader.batch_size}.')
+    logger.info(f'------Training set size: {len(train_dataset)}. Dataloader has {len(train_loader)} batches of size {train_loader.batch_size}. Learning rate is {args.lr[0]}.')
+    logger.info(f'------Clustering set size: {len(simple_train_dataset)}. Dataloader has {len(simple_train_loader)} batches of size {simple_train_loader.batch_size}.')
+    logger.info(f'------Validation set size: {len(test_dataset)}. Dataloader has {len(test_loader)} batches of size {test_loader.batch_size}.')
     logger.info(f'------Number of cell classes: {n_classes}')
     logger.info(f'------Checkpoint save path: {checkpoints_save_path}')
     logger.info(f'------Cluster pseudo-labels save path: {cluster_tmp_out}')
     logger.info(f'------Number of GPUs: {nr_gpu}')
     # Use prints_per_epoch to get iteration number to generate sample output
-    # print_frequency = len(train_loader)//prints_per_epoch;
     print_frequency_test = len(test_loader) // prints_per_epoch;
 
     best_epoch_filepath=None
     best_epoch=None
     best_f1_mean = 0
     best_prec_recall_diff = math.inf
-
-    centroids = None
     # scaler = torch.cuda.amp.GradScaler()
     
     # Define LR scheduling
@@ -259,27 +251,26 @@ if __name__=="__main__":
                     gt_kmap = gt_kmap.type(torch.FloatTensor)
 
                     # forward propagation
-                    # with torch.autocast(device_type='cuda', dtype=torch.float16):        
                     loss_dice_all, loss_dice_class, loss_dice_subclass, loss_l1_k = model(x=img,
                                                                                         gt={'gt_dmap_all':gt_dmap_all,
                                                                                             'gt_dmap':gt_dmap,
                                                                                             'gt_dmap_subclasses':gt_dmap_subclasses,
-                                                                                            'gt_kmap':{'tensor':gt_kmap, 'r_classes_all':r_classes_all}},
+                                                                                            'gt_kmap':gt_kmap},
                                                                                         loss_fns={'criterion_l1_sum':criterion_l1_sum,
-                                                                                                    'criterion_sig':criterion_sig,
-                                                                                                    'criterion_softmax':criterion_softmax})
+                                                                                                  'criterion_sig':criterion_sig,
+                                                                                                  'criterion_softmax':criterion_softmax})
                     
                     ################### MEAN REDUCTION ###################
                     loss_dice_all = loss_dice_all.mean()
                     loss_dice_class = loss_dice_class.mean()
                     loss_dice_subclass = loss_dice_subclass.mean()
-                    loss_l1_k = loss_l1_k.sum() / (gt_dmap_all.clone().sum()*r_classes_all) # fake mean reduction over pixels that should be non-zero (focus region)
+                    loss_l1_k = loss_l1_k.sum() / (gt_dmap_all.clone().sum()*k_func_dim) # fake mean reduction over pixels that should be non-zero (focus region)
                     ###########################################################################
 
                     loss_dice = lamda_class*loss_dice_class + lamda_detect*loss_dice_all + lamda_subclasses*loss_dice_subclass
 
                     # Add up the dice loss and the K function L1 loss. The K function can be NAN especially in the beginning of training. Do not add to loss if it is NAN.
-                    loss = (lamda_dice * loss_dice )
+                    loss = loss_dice 
                     if(not math.isnan(loss_l1_k.item())):
                         loss += loss_l1_k * lamda_k
                         train_count_k += 1
@@ -300,7 +291,8 @@ if __name__=="__main__":
                     epoch_loss_subclass += loss_dice_subclass.item()
 
                     # CHANGE THIS FOR TQDM UPDATES
-                    batch_loader.set_postfix({"DiceLoss": loss_dice_all.item(), "K-FuncLoss":loss_l1_k.item()})
+                    batch_loader.set_postfix({"DiceLoss": loss_dice.item(), "K-FuncLoss":loss_l1_k.item()})
+            del img, gt_dmap, gt_dmap_subclasses, gt_kmap
             empty_trash()
             epoch_loss = epoch_loss/len(train_loader)
             epoch_loss_detect = epoch_loss_detect/len(train_loader)
@@ -313,12 +305,8 @@ if __name__=="__main__":
             logger.info(f'------------train-loss-subclass: {epoch_loss_subclass:.4f}')
             logger.info(f'------------train-loss-kfunc: {epoch_loss_kfunc:.4f}')
             logger.info(f'------------train-loss: {epoch_loss:.4f}')
-        
-            #break
 
             # Testing phase on Validation Set
-            model.eval()
-            err=np.array([0 for s in range(n_classes_out)])
             loss_val = 0
             loss_val_detect = 0
             loss_val_class = 0
@@ -327,6 +315,8 @@ if __name__=="__main__":
             fp_count_all = np.zeros((n_classes_out))
             fn_count_all = np.zeros((n_classes_out))
             test_count_k = 0
+            model.eval()
+            
             with torch.no_grad():
                 with tqdm(test_loader, unit='batch', desc=f"Valid Step [{epoch}]", ascii=' =', dynamic_ncols=True) as batch_loader:
                     for (img, gt_dmap, gt_dots, gt_kmap, __) in batch_loader:
@@ -349,24 +339,23 @@ if __name__=="__main__":
                         gt_kmap = gt_kmap.type(torch.FloatTensor)
 
                         # forward Propagation
-                        # with torch.autocast(device_type='cuda', dtype=torch.float16):  
                         loss_dice_all, loss_dice_class, loss_l1_k, et_all_sig, et_class_sig = model(x=img,
                                                                                                     gt={'gt_dmap_all':gt_dmap_all,
                                                                                                         'gt_dmap':gt_dmap,
                                                                                                         'gt_dmap_subclasses':None,
-                                                                                                        'gt_kmap':{'tensor':gt_kmap, 'r_classes_all':r_classes_all}},
+                                                                                                        'gt_kmap':gt_kmap},
                                                                                                     loss_fns={'criterion_l1_sum':criterion_l1_sum,
-                                                                                                                'criterion_sig':criterion_sig,
-                                                                                                                'criterion_softmax':criterion_softmax},
+                                                                                                              'criterion_sig':criterion_sig,
+                                                                                                              'criterion_softmax':criterion_softmax},
                                                                                                     phase='test')
 
                         loss_dice_all = loss_dice_all.mean()
                         loss_dice_class = loss_dice_class.mean()
-                        loss_l1_k = (loss_l1_k.sum() / (gt_dmap_all.clone().sum()*r_classes_all)).item()
+                        loss_l1_k = (loss_l1_k.sum() / (gt_dmap_all.clone().sum()*k_func_dim)).item()
 
                         loss_dice = (lamda_class*loss_dice_class + lamda_detect*loss_dice_all).item()
 
-                        loss = (lamda_dice * loss_dice )
+                        loss = loss_dice
                         if(not math.isnan(loss_l1_k)):
                             loss += loss_l1_k * lamda_k
                             loss_val_kfunc += loss_l1_k
@@ -474,16 +463,14 @@ if __name__=="__main__":
                         f1_all[s] = 2*(precision_all[s] *recall_all[s])/(precision_all[s]+recall_all[s])
                     if s == 0:
                         logger.info(f'------Validation metrics print-out (per class):')
-                    logger.info(f'------------class {args.cell_code[s+1]}: precision_all {precision_all[s]:.4f}, recall_all {recall_all[s]:.4f}, f1_all {f1_all[s]:.4f}')
+                    logger.info(f'------------class {args.cell_code[s]}: precision_all {precision_all[s]:.4f}, recall_all {recall_all[s]:.4f}, f1_all {f1_all[s]:.4f}')
 
-                logger.info(f'------------detection: precision_all {precision_all[-1]:.4f}, recall_all {recall_all[-1]:.4f}, f1_all {f1_all[-1]:.4f}')
                 logger.info(f'------------mean classes: precision_all {precision_all[:-1].mean():.4f}, recall_all {recall_all[:-1].mean():.4f}, f1_all {f1_all[:-1].mean():.4f}')
 
 
             # Check if this is the best epoch so far based on fscore on validation set
             model_save_postfix = ''
             is_best_epoch = False
-            # if (f1_all.mean() > best_f1_mean):
             if (f1_all.mean() - best_f1_mean >= 0.005):
                 model_save_postfix += '_f1'
                 best_f1_mean = f1_all.mean()
@@ -495,7 +482,7 @@ if __name__=="__main__":
                 best_f1_mean = f1_all.mean()
                 best_prec_recall_diff = abs(recall_all.mean()-precision_all.mean())
                 is_best_epoch = True
-
+            
             # Save checkpoint if it is best so far
             if((saved == False) and (model_save_postfix != '')):
                 # print('epoch', epoch, 'saving')
@@ -508,14 +495,7 @@ if __name__=="__main__":
 
                 if(is_best_epoch):
                     best_epoch_filepath = new_epoch_filepath
-                    best_epoch = epoch
-                    
+                    best_epoch = epoch     
             # Adam optimizer needs resetting to avoid parameters learning rates dying
-            sys.stdout.flush();
-            if((epoch >= next_restart_epoch) and not(best_epoch_filepath is None)):
-                next_restart_epoch = epoch + restart_epochs_freq
-                model.load_state_dict(torch.load(best_epoch_filepath), strict=False);
-                model.to(device)
-                optimizer=torch.optim.Adam(list(model.final_layers_lst.parameters())+list(model.decoder.parameters())+list(model.bottleneck.parameters())+list(model.encoder.parameters()),args.lr) 
-
+            sys.stdout.flush();           
     logger.info('------Training complete.')
